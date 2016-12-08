dcb5316e0a0f3fb552b806274ac32e0cee733ca5
linky00 <linky00@plotholestudios.com>
2016-12-08 17:56:12 +0000

    Code less buggy, but still getting wrong result

--- 1-2.py
+++ 1-2.py
@@ -1,8 +1,14 @@
+import sys
+
 instructionsString = 'L1, L3, L5, L3, R1, L4, L5, R1, R3, L5, R1, L3, L2, L3, R2, R2, L3, L3, R1, L2, R1, L3, L2, R4, R2, L5, R4, L5, R4, L2, R3, L2, R4, R1, L5, L4, R1, L2, R3, R1, R2, L4, R1, L2, R3, L2, L3, R5, L192, R4, L5, R4, L1, R4, L4, R2, L5, R45, L2, L5, R4, R5, L3, R5, R77, R2, R5, L5, R1, R4, L4, L4, R2, L4, L1, R191, R1, L1, L2, L2, L4, L3, R1, L3, R1, R5, R3, L1, L4, L2, L3, L1, L1, R5, L4, R1, L3, R1, L2, R1, R4, R5, L4, L2, R4, R5, L1, L2, R3, L4, R2, R2, R3, L2, L3, L5, R3, R1, L4, L3, R4, R2, R2, R2, R1, L4, R4, R1, R2, R1, L2, L2, R4, L1, L2, R3, L3, L5, L4, R4, L3, L1, L5, L3, L5, R5, L5, L4, L2, R1, L2, L4, L2, L4, L1, R4, R4, R5, R1, L4, R2, L4, L2, L4, R2, L4, L1, L2, R1, R4, R3, R2, R2, R5, L1, L2'
 instructions = instructionsString.split(', ')
 direction = 0
 location = [0, 0]
 visitedLocations = []	
+
+def AddToVisited():
+	visitedLocations.append(list(location))
+
 
 for instruction in instructions:
 	if instruction[0] == 'L':
@@ -17,27 +23,26 @@ for instruction in instructions:
 	
 	moveBy = int(instruction[1:])
 	
-	print("1")
-	print(visitedLocations)
-	
-	if direction == 0:
-		location[1] += moveBy
-	elif direction == 1:
-		location[0] += moveBy
-	elif direction == 2:
-		location[1] -= moveBy
-	elif direction == 3:
-		location[0] -= moveBy
-	
-	print("2")
-	print(visitedLocations)
+	while moveBy != 0:
+		if direction == 0:
+			location[1] += 1
+			AddToVisited()
+		elif direction == 1:
+			location[0] += 1
+			AddToVisited()
+		elif direction == 2:
+			location[1] -= 1
+			AddToVisited()
+		elif direction == 3:
+			location[0] -= 1
+			AddToVisited()
+		moveBy -= 1
 
-	for visitedLocation in visitedLocations:
+	for visitedLocation in visitedLocations[:-1]:
+
 		if location == visitedLocation:
 			distance = abs(location[0]) + abs(location[1])
-			print('boo')
+			print(distance)
+			sys.exit()
 	
-	visitedLocations.append(location)
-	print("3")
-	print(visitedLocations)
 	
