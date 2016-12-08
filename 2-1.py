46ecab7d2f1a48e5a18ae17fcf619a7c4cb6d038
linky00 <linky00@plotholestudios.com>
2016-12-08 20:38:33 +0000

    Split newlines + added keypad

--- 2-1.py
+++ 2-1.py
@@ -1,2 +1,14 @@
 instructionsString = open('2-input.txt', 'r').read()
-print(instructionsString)
+instructions = instructionsString.splitlines()
+
+keypad = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9']]
+
+'''
+KEYPAD LOOKS LIKE THIS:
+ _ _ _
+|1 2 3|
+|4 5 6|
+|7 8 9|
+ - - -
+ 
+'''
