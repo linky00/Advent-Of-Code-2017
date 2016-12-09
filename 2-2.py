958a44ed9cc1849b2fb82f70e42c6898fe377696
linky00 <linky00@plotholestudios.com>
2016-12-09 17:44:14 +0000

    2-2 complete!

--- 2-2.py
+++ 2-2.py
@@ -9,25 +9,27 @@ keypad = [
 [' ', ' ', 'D', ' ', ' ']
 ]	
 
-location = [1, 1]
+location = [0,2]
 finalOutput = ''
 
 def GetNumber(inputLocation):
-	return keypad[inputLocation[1]][inputLocation[0]]
+	try:
+		return keypad[inputLocation[1]][inputLocation[0]]
+	except IndexError:
+		return None
 	
 for instructionLine in instructionsSplit:
 	instructions = list(instructionLine)
 	for instruction in instructions:
 		if instruction == 'U' and GetNumber(list([location[0], location[1] - 1])) != ' ' and location[1] > 0:
 			location[1] -= 1
-		elif instruction == 'D' and GetNumber(list([location[0], location[1] + 1])) != ' ' and location[1] < 2:
+		elif instruction == 'D' and GetNumber(list([location[0], location[1] + 1])) != ' ' and location[1] < 4:
 			location[1] += 1
 		elif instruction == 'L' and GetNumber(list([location[0] - 1, location[1]])) != ' ' and location[0] > 0:
 			location[0] -= 1
-		elif instruction == 'R' and GetNumber(list([location[0] + 1, location[1]])) != ' ' and location[0] < 2:
+		elif instruction == 'R' and GetNumber(list([location[0] + 1, location[1]])) != ' ' and location[0] < 4:
 			location[0] += 1
-		
-	print(location)
+					
 	finalOutput = finalOutput + GetNumber(location)
 	
 print(finalOutput)
