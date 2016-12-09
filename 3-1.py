c22d090918c6acb120b2542c4fa08ffa1b048745
linky00 <linky00@plotholestudios.com>
2016-12-09 23:28:37 +0000

    We're getting there... kinda...

--- 3-1.py
+++ 3-1.py
@@ -1,2 +1,13 @@
-instructionsString = open('3-input.txt', 'r').read()
-instructionsSplit = instructionsString.splitlines
+trianglesString = open('3-input.txt', 'r').read()
+trianglesSplit = trianglesString.splitlines()
+triangles = []
+for triangleToArray in trianglesSplit:
+	triangles.append(triangleToArray.split(' '))
+
+for triangle in triangles:
+	for length in triangle:
+		if length == '':
+			del length
+		length = int(length)
+
+print(triangles)
